084afe7cb98844b726962f524dfc807717991568
CSPEECE1296 <cspeece1296@stu.neisd.net>
2023-08-21 16:01:07 -0500

    Lessons Created.

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner
-        Scanner scan = new Scanner(System.in);
-
-        // Store/Input
-        System.out.println("Enter a name:");
-        String name = scan.nextLine();
-
-        // Check
-        if (name.equals("Hello")) {
-            // Output
-            System.out.printf("%s is not a name", name);
-            System.out.println("\t\t\t\t\t\nNot funny.");
-        }
+        System.out.println("=========");
+        System.out.print("= 0   0 =\tJava is Awesome!\n");
+        System.out.println("=   v   =");
+        System.out.print("=========");
     }
 }
