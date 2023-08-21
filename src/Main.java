8630d30b1721b75cd14b699643fac7f63044c55a
CSPEECE1296 <cspeece1296@stu.neisd.net>
2023-08-21 15:37:43 -0500

    Lessons Created.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Lessons
+
+// Import
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Store/Input
+        System.out.println("Enter a name");
+        String name = scan.nextLine();
+    }
+}
