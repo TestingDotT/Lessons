65eb4c2e4347f0a617f9d27d3c64fed03d4eb25a
CSPEECE1296 <cspeece1296@stu.neisd.net>
2023-08-23 15:55:45 -0500

    Lessons Created.

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,6 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         // Store/Input
-        System.out.println("Hi there. What is your name?");
-        String name = scan.nextLine();
-        System.out.println("What state do you live in?");
-        String state = scan.nextLine();
-
-        // Output
-        System.out.printf("My name is %s. I live in %s.", name, state);
-
-        /*
-        // Store/Input
-        System.out.println("Please enter four names:");
-        String nameOne = scan.nextLine();
-        String nameTwo = scan.nextLine();
-        String nameThree = scan.nextLine();
-        String nameFour = scan.nextLine();
-
-        // Output
-        System.out.printf("%s %s %s %s" + nameFour, nameTwo, nameThree, nameOne);
-
-        scan.close();
-         */
+        System.out.println("");
     }
 }
