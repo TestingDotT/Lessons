a15c0c1eb593aebaf3cbd267f46f99afea96240c
CSPEECE1296 <cspeece1296@stu.neisd.net>
2023-08-25 15:40:53 -0500

    Calculating Grades v1.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 // Lessons
 
 // Import
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.*;
 
 public class Main {
@@ -9,6 +11,26 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         // Store/Input
+        System.out.println("Please enter the course name.");
+        String courseName = scan.nextLine();
+
+        System.out.println("Please enter the average time spent in a week for this course in minutes.");
+        int averageTimeMin = scan.nextInt();
+
+        System.out.println("Please enter the homework grades for this course.");
+        int gradeOne = scan.nextInt();
+        int gradeTwo = scan.nextInt();
+        int gradeThree = scan.nextInt();
+        int gradeFour = scan.nextInt();
+
+        System.out.println("Please enter the quiz grades for this course.");
+        double quizOne = scan.nextDouble();
+        double quizTwo = scan.nextDouble();
+
+        System.out.println("Please enter the final exam grade for this course.");
+        double finalExamGrade = scan.nextDouble();
+
+        // Output
         System.out.println("");
     }
 }
